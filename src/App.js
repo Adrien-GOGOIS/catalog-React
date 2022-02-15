321f0cd5e246cd160504405517174f95064c7b81
Adrien GOGOIS <ad.gogois@gmail.com>
2022-02-15 14:41:53 +0100

    Map movies.jsom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import Movies from "./views/Movies.js";
 import "./App.css";
 
+import movies from "./films.json";
+
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+
+const data = movies.map((movie) => {
+  return <Movies title={movie.title} />;
+});
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <div></div>
+        {/* {data} */}
         <Switch>
           <Route exact path="/" component={App} />
           <Route path="/movies/:name" component={Movies} />
--- src/views/Movies.js
+++ src/views/Movies.js
@@ -4,11 +4,15 @@ class Movies extends React.Component {
   render() {
     return (
       <div>
-        <p>{this.props.title}</p>
-        <p>{this.props.director}</p>
-        <p>{this.props.stars}</p>
-        <p>{this.props.image}</p>
-        <p>{this.props.description}</p>
+        <ul>
+          <li>
+            <p>{this.props.title}</p>
+            <p>{this.props.director}</p>
+            <p>{this.props.stars}</p>
+            <p>{this.props.image}</p>
+            <p>{this.props.description}</p>
+          </li>
+        </ul>
       </div>
     );
   }
